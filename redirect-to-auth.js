374c508521bbcef70f18a8e6b044e6c2f025fe40
Jim Kang <jimkang@gmail.com>
2017-11-12 15:34:11 -0500

    Formatting.

--- redirect-to-auth.js
+++ redirect-to-auth.js
@@ -1,16 +1,24 @@
 var qs = require('qs');
 
-function redirectToAuth({routeDict, clientId, scopes}) {
-  var redirectURI = window.location.protocol + '//' +
-    window.location.host + window.location.pathname;
+function redirectToAuth({ routeDict, clientId, scopes }) {
+  var redirectURI =
+    window.location.protocol +
+    '//' +
+    window.location.host +
+    window.location.pathname;
 
   var originalRoute = qs.stringify(routeDict);
-  var authURI = 'https://github.com/login/oauth/authorize?' +
-    'client_id=' + clientId +
+  var authURI =
+    'https://github.com/login/oauth/authorize?' +
+    'client_id=' +
+    clientId +
     '&response_type=token' +
-    '&scope=' + encodeURIComponent(scopes.join(' ')) +
-    '&redirect_uri=' + encodeURIComponent(redirectURI) +
-    '&state=' + encodeURIComponent(originalRoute);
+    '&scope=' +
+    encodeURIComponent(scopes.join(' ')) +
+    '&redirect_uri=' +
+    encodeURIComponent(redirectURI) +
+    '&state=' +
+    encodeURIComponent(originalRoute);
 
   // Note: We're using state to restore hash state, not for security here.
 
